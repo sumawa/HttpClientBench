77e464e007da0da88f8516521ca95321fa28da39
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-03-29 19:12:07 +0530

    increasing timeouts for testing

--- run_client_benchmark.sh
+++ run_client_benchmark.sh
@@ -1,3 +1,5 @@
 docker-compose stop zhttp-server zhttp-client
 docker-compose rm -f zhttp-server zhttp-client
-docker-compose up -d
+docker-compose up -d zhttp-server
+docker-compose up zhttp-client
+docker-compose down zhttp-server
