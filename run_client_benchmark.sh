6da0eef0e2778933cbb81c48ce55cf02b5b43017
Ubuntu <ubuntu@ip-172-31-21-10.ec2.internal>
2022-03-29 16:35:13 +0000

    increase number of retries

--- run_client_benchmark.sh
+++ run_client_benchmark.sh
@@ -2,4 +2,4 @@ docker-compose stop zhttp-server zhttp-client
 docker-compose rm -f zhttp-server zhttp-client
 docker-compose up -d zhttp-server
 docker-compose up zhttp-client
-docker-compose down zhttp-server
+docker-compose stop zhttp-server zhttp-client
