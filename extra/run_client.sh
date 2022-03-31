ee8e69aa92d4d93042ffb2c7b7bbddeb40c253c7
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-03-31 19:27:32 +0530

    one consolidated results log

--- extra/run_client.sh
+++ extra/run_client.sh
@@ -2,4 +2,4 @@ rm -rf /log/*client*.log
 
 clientArray=("SttpStandalone" "Zhttp" "Http4s")
 
-for T in ${clientArray[@]} ; do CLIENT_TYPE=$T java -classpath /target/scala-2.13/client-benchmark-assembly-0.1.0-SNAPSHOT.jar bench.${T}Benchmark >> /log/${T}.out ; done
+for T in ${clientArray[@]} ; do CLIENT_TYPE=$T java -classpath /target/scala-2.13/client-benchmark-assembly-0.1.0-SNAPSHOT.jar bench.${T}Benchmark >> /log/results.out ; done
