c50b5c221ad79c20b3ed9bd06014edd1e28db68b
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-03-31 10:33:20 +0530

    added time lapse to identify slowest process

--- extra/run_server.sh
+++ extra/run_server.sh
@@ -1,11 +1,13 @@
 rm -rf /log/*.log
 rm -rf /log/*.out
-#echo "$(pwd)" >> /log/server_out.log
 echo "BUILDING ASSEMBLY IN SERVER" >> /log/server_out.log
 cd /
+SECONDS=0
 export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xmx2G -Xms1G"
-# sbt assembly >> /log/assembly.log
-echo "DONE BUILDING ASSEMBLY" >> /log/server_out.log
+#sbt assembly >> /log/assembly.log
+sbt assembly >> /log/server_out.log
+duration=$SECONDS
+echo "DONE BUILDING ASSEMBLY $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." >> /log/server_out.log
 
 java -classpath /target/scala-2.13/client-benchmark-assembly-0.1.0-SNAPSHOT.jar bench.SimpleServer >> /log/out.log 
 
