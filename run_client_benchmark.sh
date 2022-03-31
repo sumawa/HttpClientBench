45485dac0abde49758f0b9cca4088e4df411b5c6
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-03-31 10:51:05 +0530

    commented removal of containers

--- run_client_benchmark.sh
+++ run_client_benchmark.sh
@@ -1,5 +1,5 @@
 docker-compose stop zhttp-server zhttp-client
-docker-compose rm -f zhttp-server zhttp-client
+#docker-compose rm -f zhttp-server zhttp-client
 docker-compose up -d zhttp-server
 docker-compose up zhttp-client
 docker-compose stop zhttp-server zhttp-client
